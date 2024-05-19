a27191623b384e3208208cbafdbdc735abeb3bf5
miyoon17 <153158257+miyoon17@users.noreply.github.com>
2024-05-19 22:37:45 +0900

    model.jsコード作成

--- model.js
+++ model.js
@@ -1,13 +1,20 @@
 class TaskModel {
   constructor() {
-    this.tasks = [];
+    this.tasks = []; // タスクを保存するための空の配列
   }
 
   addTask(task) {
-    this.tasks.push(task);
+    this.tasks.push(task); //新しいタスクを配列に追加
   }
 
   deleteTask(task) {
-    this.tasks = this.tasks.filter(t => t !== task);
+    this.tasks = this.tasks.filter(t => t !== task);  //指定されていたタスクを配列から削除
+  }
+
+  getTask () {
+    return this.tasks;  //全てのタスクを返す
+  }
 }
-}
+
+const taskModel = new TaskModel();  //TaskModelクラスのインスタンスを作成
+export default taskModel;  //インスタンスをエクスポート
